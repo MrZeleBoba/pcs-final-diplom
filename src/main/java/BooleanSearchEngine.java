30bd4f098e2fb3e8af3a25e80b3c0b32f0e642b6
Aleksey Galishchev <galishchev@inbox.ru>
2023-05-03 19:30:11 +0300

    Correct Work

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,5 +1,4 @@
 import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
@@ -8,29 +7,32 @@ import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    private final Map<PageEntry, String> wordList = new HashMap<>();
+    private final Map<String, List<PageEntry>> database = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        File[] files = pdfsDir.listFiles();
-        if (files != null && files.length > 0) {
-            for (File file : files) {
-                if (file.getName().contains(".pdf")) {
-                    PdfDocument document = new PdfDocument(new PdfReader(file));
-                    for (int i = 0; i < document.getNumberOfPages(); i++) {
-                        PdfPage page = document.getPage(i + 1);
-                        String text = PdfTextExtractor.getTextFromPage(page);// получить текст со страницы
-                        String[] words = text.split("\\P{IsAlphabetic}+");// разбить текст на слова
-                        Map<String, Integer> freqs = new HashMap<>();
-                        for (var word : words) {
-                            if (word.isEmpty()) {
-                                continue;
-                            }
-                            int f = freqs.getOrDefault(word, 0) + 1;
-                            freqs.put(word.toLowerCase(), f);
-                            wordList.put(new PageEntry(file.getName(), document.getPageNumber(page), f), word.toLowerCase());
-                        }
+        for (File pdf : pdfsDir.listFiles()) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            int pageCount = doc.getNumberOfPages();
+            for (int i = 1; i <= pageCount; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+                for (var word : words) { // перебираем слова
+                    if (word.isEmpty()) {
+                        continue;
                     }
-                    document.close();
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                for (String word : freqs.keySet()) {
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
+                    if (database.containsKey(word)) {
+                        database.get(word).add(pageEntry);
+                    } else {
+                        database.put(word, new ArrayList<>());
+                        database.get(word).add(pageEntry);
+                    }
                 }
             }
         }
@@ -38,14 +40,8 @@ public class BooleanSearchEngine implements SearchEngine {
 
     @Override
     public List<PageEntry> search(String word) {
-        List<PageEntry> list = new ArrayList<>();
-        for (Map.Entry<PageEntry, String> entry : wordList.entrySet()) {
-            if (entry.getValue().equals(word)) {
-                list.add(entry.getKey());
-            }
-        }
-        Collections.sort(list);
-        return list;
+        List<PageEntry> result = database.get(word);
+        Collections.sort(result);
+        return result;
     }
-
 }
